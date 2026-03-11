75c598f29448e23c566f55a5761e039969db1786
rgrin08 <160621511+rgrin08@users.noreply.github.com>
2026-03-11 10:56:51 +0100

    Test de renvoi d'une erreur interne du serveur.

--- src/main/java/fr/richardgrin/tp3haiti202526/HelloResource.java
+++ src/main/java/fr/richardgrin/tp3haiti202526/HelloResource.java
@@ -4,13 +4,15 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 
-@Path("/hello-world")
+ @Path("/hello-world")
 public class HelloResource {
     @GET
     @Produces("text/plain")
     @Path("/personnes/{nom}")
-    public String hello(@PathParam("nom") String nom) {
-        return "Hello, " + nom + "!";
+    public Response hello(@PathParam("nom") String nom) {
+//        return Response.ok("Hello, " + nom + "!").build();
+        return Response.serverError().build();
     }
 }
