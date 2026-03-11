0946278846f90af975a0a45bf6978a0fc691ce4c
rgrin08 <160621511+rgrin08@users.noreply.github.com>
2026-03-11 10:09:05 +0100

    Etat initial du projet.

---
+++ src/main/java/fr/richardgrin/tp3haiti202526/HelloResource.java
@@ -0,0 +1,14 @@
+ package fr.richardgrin.tp3haiti202526;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
