bf1d897d931c8c090f46656cd9d1b8d0a1fc2f35
rgrin08 <160621511+rgrin08@users.noreply.github.com>
2026-03-11 10:46:36 +0100

    Ajout d'un paramètre à la méthode de HelloResource.

--- src/main/java/fr/richardgrin/tp3haiti202526/HelloResource.java
+++ src/main/java/fr/richardgrin/tp3haiti202526/HelloResource.java
@@ -2,13 +2,15 @@
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 
 @Path("/hello-world")
 public class HelloResource {
     @GET
     @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Path("/personnes/{nom}")
+    public String hello(@PathParam("nom") String nom) {
+        return "Hello, " + nom + "!";
     }
 }
